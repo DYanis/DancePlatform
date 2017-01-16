32a904ebc9eebc5181233272e7c8e6814f8181c9
dyanis <dimitar.yanis@yahoo.com>
2017-01-16 17:22:02 +0200

    added login with facebook - AppId and AppSecret storeted in secret manager

--- src/DancePlatform/Startup.cs
+++ src/DancePlatform/Startup.cs
@@ -108,6 +108,12 @@ namespace DancePlatform
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
